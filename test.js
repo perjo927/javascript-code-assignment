4cd9698ef17324d5fa28f518a40284b8d693e6ba
ladbon <ladbon.m@gmail.com>
2023-11-15 11:45:30 +0100

    Completed the assignment

--- test.js
+++ test.js
@@ -1,49 +1,46 @@
 var assert = require('assert');
 
-// ASSIGNMENT CODE BEGIN //
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let squaredEvenNumbers = [];
-const squaredOddNumbers = [];
+// Function to square numbers and categorize them
+// Här tänkte jag att jag ville nog ändra detta till två olika funktioner först
+// det skulle öka modulariteten och öka läsbarheten. Men jag säger det hellre 
+// än att faktiskt göra det. Bättre att prata om det senare. 
 
-function squareNumbers() {
-    for (i = 0; i < numbers.length; i++) {
-        if (i % 2 == 0) {
-            squaredEvenNumbers.push(numbers[i] * numbers[i]);
+function squareAndCategorizeNumbers(numbers) {
+    let squaredNumbers = {
+        odd: [],
+        even: [],
+    };
+
+    for (let i = 0; i < numbers.length; i++) {
+        const squared = numbers[i] * numbers[i];
+        if (squared % 2 === 0) {
+            squaredNumbers.even.push(squared);
         } else {
-            squaredOddNumbers.push(numbers[i] * numbers[i]);
+            squaredNumbers.odd.push(squared);
         }
     }
 
-    return
-    {
-        odd: squaredOddNumbers,
-            even: squaredEvenNumbers,
-  };
+    return squaredNumbers;
 }
 
-const squares = squareNumbers();
-// ASSIGNMENT CODE END //
+// Calculate the sum of an array
+const sum = (list) => list.reduce((acc, curr) => acc + curr, 0);
 
-// DEBUG HELPER //
-console.table(squares);
-
-// Should produce the following result:
-// ┌─────────┬───┬───┬────┬────┬────┐
-// │ (index) │ 0 │ 1 │ 2  │ 3  │ 4  │
-// ├─────────┼───┼───┼────┼────┼────┤
-// │   odd   │ 1 │ 9 │ 25 │ 49 │ 81 │
-// │  even   │ 0 │ 4 │ 16 │ 36 │ 64 │
-// └─────────┴───┴───┴────┴────┴────┘
-
-// UNIT TESTS //
-// These tests must pass! But feel free to refactor :)
+// Unit tests
+// Nu ligger numbers här för det är egentligen test data
+// Nu kallar du till funktionen i sig och testar med ett dataset istället för att göra
+// det utanför testramen. På det viset är det tydligare vad vi gör och testar samt
+// modulärt att ta ut funktionen och använda den någon annanstans. 
 describe('Squared numbers', function () {
-    const sum = (list) => list.reduce((acc, curr) => acc + curr, 0);
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     it('should return odd numbers', function () {
-        assert.equal(sum([1, 9, 25, 49, 81]), sum(squares.odd));
+        const squaredNumbers = squareAndCategorizeNumbers(numbers);
+        assert.equal(sum([1, 9, 25, 49, 81]), sum(squaredNumbers.odd));
     });
+
     it('should return even numbers', function () {
-        assert.equal(sum([0, 4, 16, 36, 64]), sum(squares.even));
+        const squaredNumbers = squareAndCategorizeNumbers(numbers);
+        assert.equal(sum([0, 4, 16, 36, 64]), sum(squaredNumbers.even));
     });
 });
